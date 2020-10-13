340fdc28c4087b6338d810987474350b833d0f4c
Yukihirai777 <72774963+Yukihirai777@users.noreply.github.com>
2020-10-13 12:14:05 +0900

    Changesection1-1
    I had to make a lot of efforts regardless of beginning.

--- section1-1/sketch.js
+++ section1-1/sketch.js
@@ -5,12 +5,12 @@ function setup() {
 function draw() {
   background(0);
   strokeWeight(4);
-  fill(178, 100, 162);
+  fill(100, 200, 200);
   triangle(12,120,116,120,116,10);
-  fill(94, 185, 84);
+  fill(150, 150, 150);
   triangle(0,108,106,108,106,0);
   fill(255);
   textSize(32);
   textFont("serif");
-  text("46", 68, 100);
+  text("77", 68, 100);
 }
